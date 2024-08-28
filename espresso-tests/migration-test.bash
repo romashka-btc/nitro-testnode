6b9cda30a7c93e966164998b5b568eacd2749772
sveitser <sveitser@gmail.com>
2024-08-28 13:46:34 +0200

    Deploy an UpgradeExecutor on child chain
    - Add `--tokenbridge` to get an UpgradeExecutor on child chain. This
      allows us to execute the Actions on the child chain.
    - Add pending check to verify staker is making progress.
    - Decode return values from cast by specifying return type.
    - Remove `--private-key` argument for cast calls.
    - Simplify the check for updated balances.

--- espresso-tests/migration-test.bash
+++ espresso-tests/migration-test.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 fail(){
-    echo "$*" 1>&2; exit -1;
+    echo "$*" 1>&2; exit 1;
 }
 
 set -euo pipefail
@@ -18,7 +18,7 @@ ORBIT_ACTIONS_DIR="$TESTNODE_DIR/orbit-actions"
 cd "$TESTNODE_DIR"
 
 # Initialize a standard network not compatible with espresso
-./test-node.bash --simple --init-force --detach
+./test-node.bash --simple --init-force --tokenbridge --detach
 
 # Start espresso sequencer node
 docker compose up espresso-dev-node --detach
@@ -27,7 +27,12 @@ docker compose up espresso-dev-node --detach
 . "$TEST_DIR/.env"
 
 # Overwrite the ROLLUP_ADDRESS for this test, it might not be the same as the one in the .env file
-ROLLUP_ADDRESS=$(docker compose run --entrypoint sh poster -c "jq -r '.[0].rollup.rollup' /config/deployed_chain_info.json | tail -n 1 | tr -d '\r\n'")
+ROLLUP_ADDRESS=$(docker compose run --entrypoint cat scripts /config/deployed_chain_info.json | jq -r '.[0].rollup.rollup' | tail -n 1 | tr -d '\r\n')
+
+# A convoluted way to get the address of the child chain upgrade executor, maybe there's a better way?
+INBOX_ADDRESS=$(docker compose run --entrypoint cat scripts /config/deployed_chain_info.json | jq -r '.[0].rollup.inbox' | tail -n 1 | tr -d '\r\n')
+L1_TOKEN_BRIDGE_CREATOR_ADDRESS=$(docker compose run --entrypoint cat scripts /tokenbridge-data/network.json | jq -r '.l1TokenBridgeCreator' | tail -n 1 | tr -d '\r\n')
+CHILD_CHAIN_UPGRADE_EXECUTOR_ADDRESS=$(cast call $L1_TOKEN_BRIDGE_CREATOR_ADDRESS 'inboxToL2Deployment(address)(address,address,address,address,address,address,address,address,address)' $INBOX_ADDRESS | tail -n 2 | head -n 1 | tr -d '\r\n')
 
 # Export l2 owner private key and address
 PRIVATE_KEY="$(docker compose run scripts print-private-key --account l2owner | tail -n 1 | tr -d '\r\n')"
@@ -66,6 +71,7 @@ cast send $UPGRADE_EXECUTOR "execute(address, bytes)" $OSP_MIGRATION_ACTION $(ca
 
 echo "Executed OspMigrationAction via UpgradeExecutor"
 
+NUM_CONFIRMED_NODES_BEFORE_UPGRADE=$(cast call --rpc-url $RPC_URL $ROLLUP_ADDRESS 'latestConfirmed()(uint256)')
 #shutdown nitro node
 docker stop nitro-testnode-sequencer-1
 
@@ -79,6 +85,9 @@ while ! curl -s $L2_RPC_URL > /dev/null; do
   sleep 5
 done
 
+echo "Adding child chain upgrade executor as an L2 chain owner"
+cast send 0x0000000000000000000000000000000000000070 'addChainOwner(address)' $CHILD_CHAIN_UPGRADE_EXECUTOR_ADDRESS --rpc-url $L2_RPC_URL --private-key $PRIVATE_KEY
+
 echo "Deploying ArbOS action"
 
 #forge script to deploy the Espresso ArbOS upgrade action.
@@ -88,50 +97,50 @@ ARBOS_UPGRADE_ACTION=$(cat broadcast/DeployArbOSUpgradeAction.s.sol/412346/run-l
 echo "Deployed ArbOSUpgradeAction at $ARBOS_UPGRADE_ACTION"
 
 # TODO: figure out why this upgrade does not do anything
-# cast send $UPGRADE_EXECUTOR "execute(address, bytes)" $ARBOS_UPGRADE_ACTION $(cast calldata "perform()") --rpc-url $L2_RPC_URL --private-key $PRIVATE_KEY
+cast send $CHILD_CHAIN_UPGRADE_EXECUTOR_ADDRESS "execute(address, bytes)" $ARBOS_UPGRADE_ACTION $(cast calldata "perform()") --rpc-url $L2_RPC_URL --private-key $PRIVATE_KEY
 
-ARBOS_VERSION_BEFORE_UPGRADE=$(cast call "0x0000000000000000000000000000000000000064" "arbOSVersion()" --rpc-url $L2_RPC_URL --private-key $PRIVATE_KEY)
+ARBOS_VERSION_BEFORE_UPGRADE=$(cast call "0x0000000000000000000000000000000000000064" "arbOSVersion()(uint64)" --rpc-url $L2_RPC_URL)
 
-# TODO: remove manually calling the pre-compile
-cast send 0x0000000000000000000000000000000000000070 'scheduleArbOSUpgrade(uint64,uint64)' 35 0 --rpc-url http://localhost:8547 --private-key $PRIVATE_KEY
+# Check the upgrade happened
 
-#check the upgrade happened
-
-ARBOS_VERSION_AFTER_UPGRADE=$(cast call "0x0000000000000000000000000000000000000064" "arbOSVersion()" --rpc-url $L2_RPC_URL --private-key $PRIVATE_KEY)
+ARBOS_VERSION_AFTER_UPGRADE=$(cast call "0x0000000000000000000000000000000000000064" "arbOSVersion()(uint64)" --rpc-url $L2_RPC_URL)
 
 while [ $ARBOS_VERSION_BEFORE_UPGRADE == $ARBOS_VERSION_AFTER_UPGRADE ]
 do
   sleep 5
-  ARBOS_VERSION_AFTER_UPGRADE=$(cast call "0x0000000000000000000000000000000000000064" "arbOSVersion()" --rpc-url $L2_RPC_URL --private-key $PRIVATE_KEY)
+  ARBOS_VERSION_AFTER_UPGRADE=$(cast call "0x0000000000000000000000000000000000000064" "arbOSVersion()(uint64)" --rpc-url $L2_RPC_URL)
 done
 
-#The arbsys precompile is returning 55 for the arbos version which indicates that the value internally is 0
-
-if [ $ARBOS_VERSION_AFTER_UPGRADE != "0x000000000000000000000000000000000000000000000000000000000000005a" ]; then
-  fail "ArbOS version not updated: Expected 5a, Actual $ARBOS_VERSION_AFTER_UPGRADE"
+# The arbsys precompile is returning 55 for the arbos version which indicates that the value internally is 0
+# We are upgrading the ArbOS version to 35 so the expect the return value to be 55 + 35 = 90
+if [ $ARBOS_VERSION_AFTER_UPGRADE != "90" ]; then
+  fail "ArbOS version not updated: Expected 90, Actual $ARBOS_VERSION_AFTER_UPGRADE"
 fi
 
 #test for new OSP address
-OSP_ADDR=$(cast call $CHALLENGE_MANAGER_ADDRESS $(cast calldata "osp()") --rpc-url $RPC_URL --private-key $PRIVATE_KEY)
-
-if [ $NEW_OSP_ENTRY != $OSP_ADDR ]; then
-  fail "OSP has not been set to newly deployed OSP: \n Newly deployed: $NEW_OSP_ENTRY \n Currently set OSP: $OSP_ADDR"
+CHALLENGE_MANAGER_OSP_ADDRESS=$(cast call $CHALLENGE_MANAGER_ADDRESS "osp()(address)" --rpc-url $RPC_URL)
+if [ $NEW_OSP_ENTRY != $CHALLENGE_MANAGER_OSP_ADDRESS ]; then
+  fail "OSP has not been set to newly deployed OSP: \n Newly deployed: $NEW_OSP_ENTRY \n Currently set OSP: $CHALLENGE_MANAGER_OSP_ADDRESS"
 fi
 # check for balance update and transactions actually being sequenced
-ORIGINAL_OWNER_BALANCE=$(cast balance $OWNER_ADDRESS -e)
+ORIGINAL_OWNER_BALANCE=$(cast balance $OWNER_ADDRESS -e --rpc-url $L2_RPC_URL)
 
-docker compose run scripts send-l2 --ethamount 100 --to l2owner --wait
+# Send 1 eth as the owner
+RECIPIENT_ADDRESS=0xAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+BALANCE_ORIG=$(cast balance $RECIPIENT_ADDRESS -e --rpc-url $L2_RPC_URL)
+cast send $RECIPIENT_ADDRESS --value 1ether --rpc-url $L2_RPC_URL --private-key $PRIVATE_KEY
 
-NEW_OWNER_BALANCE=$(cast balance $OWNER_ADDRESS -e)
+BALANCE_NEW=$(cast balance $RECIPIENT_ADDRESS -e --rpc-url $L2_RPC_URL)
 
-if [ $(($ORIGINAL_OWNER_BALANCE + 100)) != $NEW_OWNER_BALANCE ]; then
-  fail "Owner balance should have increased: Expected amount: $(($ORIGINAL_OWNER_BALANCE + 100)), Actual ammount: $NEW_OWNER_BALANCE"
+if [ $BALANCE_NEW == $BALANCE_ORIG ]; then
+  fail "Balance of $RECIPIENT_ADDRESS should have changed but remained: $BALANCE_ORIG"
 fi
+echo "Balance of $RECIPIENT_ADDRESS changed from $BALANCE_ORIG to $BALANCE_NEW"
 
-docker compose run scripts send-l2 --ethamount 100 --to l2owner --wait
+# TODO: check that the staker is making progress after the upgrade
+while [ "$NUM_CONFIRMED_NODES_BEFORE_UPGRADE" == "$(cast call --rpc-url $RPC_URL $ROLLUP_ADDRESS 'latestConfirmed()(uint256)')" ]; do
+  echo "Waiting for confirmed nodes ..."
+  sleep 5
+done
 
-FINAL_OWNER_BALANCE=$(cast balance $OWNER_ADDRESS -e)
-
-if [ $(($NEW_OWNER_BALANCE + 100)) != $FINAL_OWNER_BALANCE ]; then
-  fail "Owner balance should have increased: Expected amount: $(($ORIGINAL_OWNER_BALANCE + 100)), Actual ammount: $NEW_OWNER_BALANCE"
-fi
+echo "Confirmed nodes have progressed"
