884a63cd9895295ea06d864f7e2e1e7741529141
Sneh Koul <35871990+Sneh1999@users.noreply.github.com>
2024-09-24 11:25:24 -0400

    Use the tag before marketplace update (#55)
    * merge
    
    * use latest instead of integration

--- test-node.bash
+++ test-node.bash
@@ -9,7 +9,7 @@ DEFAULT_NITRO_CONTRACTS_REPO="https://github.com/OffchainLabs/nitro-contracts.gi
 DEFAULT_NITRO_CONTRACTS_VERSION="99c07a7db2fcce75b751c5a2bd4936e898cda065"
 DEFAULT_TOKEN_BRIDGE_VERSION="v1.2.2"
 
-ESPRESSO_VERSION=ghcr.io/espressosystems/nitro-espresso-integration/nitro-node-dev:integration
+ESPRESSO_VERSION=ghcr.io/espressosystems/nitro-espresso-integration/nitro-node-dev:latest
 
 # Set default versions if not overriden by provided env vars
 : ${NITRO_CONTRACTS_REPO:=$DEFAULT_NITRO_CONTRACTS_REPO}
